72468d4c34d4d6cd27e07835a39dfa8998e443f9
Tao Wang <hellotaotao@gmail.com>
2019-02-28 15:00:44 +1030

    auto format with PHPStorm

--- upload/catalog/controller/extension/payment/humm.php
+++ upload/catalog/controller/extension/payment/humm.php
@@ -1,4 +1,5 @@
 <?php
+
 class ControllerExtensionPaymentHumm extends Controller {
     const IS_DEBUG = false;
 
@@ -7,96 +8,101 @@ class ControllerExtensionPaymentHumm extends Controller {
      *
      * @return void
      */
-    public function __construct($registry) {
-        parent::__construct($registry);
+    public function __construct( $registry ) {
+        parent::__construct( $registry );
 
-		$this->load->language('extension/payment/humm');
-        $this->load->model('extension/payment/humm');
-        $this->load->model('checkout/order');
+        $this->load->language( 'extension/payment/humm' );
+        $this->load->model( 'extension/payment/humm' );
+        $this->load->model( 'checkout/order' );
     }
 
     /**
      * @return string
      */
     public function index() {
-        if ($this->cart->getTotal() >= 20) {
-            $data['button_confirm'] = $this->language->get('button_confirm');
+        if ( $this->cart->getTotal() >= 20 ) {
+            $data['button_confirm'] = $this->language->get( 'button_confirm' );
 
-            $data['text_loading'] = $this->language->get('text_loading');
+            $data['text_loading'] = $this->language->get( 'text_loading' );
 
             $data['params'] = $this->model_extension_payment_humm->getParams();
 
             $data['action'] = $this->model_extension_payment_humm->getGatewayUrl();
         } else {
-            $data['error'] = sprintf($this->language->get('error_amount'), $this->currency->format(20, $this->session->data['currency'], 1));
+            $data['error'] = sprintf( $this->language->get( 'error_amount' ), $this->currency->format( 20, $this->session->data['currency'], 1 ) );
         }
-        return $this->load->view('extension/payment/humm', $data);
+
+        return $this->load->view( 'extension/payment/humm', $data );
     }
 
     /**
      * @return void
      */
     public function callback() {
-        $this->debugLogIncoming('Callback');
+        $this->debugLogIncoming( 'Callback' );
 
         // Validate Response
         try {
-            $order_info = $this->getOrderAndVerifyResponse($this->request->post);
-        } catch (\Exception $e) {
+            $order_info = $this->getOrderAndVerifyResponse( $this->request->post );
+        } catch ( \Exception $e ) {
             // Handle callback error
             $reference_id = "";
-            if (isset($this->request->post['x_reference'])){
+            if ( isset( $this->request->post['x_reference'] ) ) {
                 $reference_id = $this->request->post['x_reference'];
             }
-            return $this->callbackBadRequest($reference_id, $e->getMessage());
+
+            return $this->callbackBadRequest( $reference_id, $e->getMessage() );
         }
 
-        $result = $this->updateOrder($order_info, $this->request->post);
+        $result = $this->updateOrder( $order_info, $this->request->post );
 
-        $this->response->addHeader('Content-type: application/json');
-        $this->response->setOutput(json_encode(['reference_id' => $this->request->post['x_reference'], 'status' => $result]));
+        $this->response->addHeader( 'Content-type: application/json' );
+        $this->response->setOutput( json_encode( [
+            'reference_id' => $this->request->post['x_reference'],
+            'status'       => $result
+        ] ) );
     }
 
     /**
      * @return void
      */
     public function complete() {
-        $this->debugLogIncoming('Complete');
+        $this->debugLogIncoming( 'Complete' );
 
         // Validate Response
         try {
-            $order_info = $this->getOrderAndVerifyResponse($this->request->get);
-        } catch (\Exception $e) {
+            $order_info = $this->getOrderAndVerifyResponse( $this->request->get );
+        } catch ( \Exception $e ) {
             // Give the customer a general error
-            $this->session->data['error'] = $this->language->get('text_transaction_verification');
+            $this->session->data['error'] = $this->language->get( 'text_transaction_verification' );
 
-            $this->response->redirect($this->url->link('checkout/checkout', '', true));
+            $this->response->redirect( $this->url->link( 'checkout/checkout', '', true ) );
 
             return;
         }
 
-        $this->updateOrder($order_info, $this->request->get);
+        $this->updateOrder( $order_info, $this->request->get );
 
         // Failed transaction outcome
-        if ($this->request->get['x_result'] == 'failed') {
-            $this->session->data['error'] = $this->language->get('text_transaction_failed');
+        if ( $this->request->get['x_result'] == 'failed' ) {
+            $this->session->data['error'] = $this->language->get( 'text_transaction_failed' );
 
-            $this->response->redirect($this->url->link('checkout/checkout', '', true));
+            $this->response->redirect( $this->url->link( 'checkout/checkout', '', true ) );
         }
 
         // Success!
-        $this->response->redirect($this->url->link('checkout/success', '', true));
+        $this->response->redirect( $this->url->link( 'checkout/success', '', true ) );
     }
 
     /**
      * @return void
      */
     public function cancel() {
-        $this->debugLogIncoming('Cancel');
+        $this->debugLogIncoming( 'Cancel' );
 
-        $this->session->data['error'] = $this->language->get('text_transaction_cancelled');
+        $this->session->data['error'] = $this->language->get( 'text_transaction_cancelled' );
 
-        $this->response->redirect($this->url->link('checkout/checkout', '', true));
+        $this->response->redirect( $this->url->link( 'checkout/checkout', '', true ) );
     }
 
     /**
@@ -104,19 +110,19 @@ class ControllerExtensionPaymentHumm extends Controller {
      *
      * @return void
      */
-    private function callbackBadRequest($reference_id, $comment) {
+    private function callbackBadRequest( $reference_id, $comment ) {
         $params = [];
 
-        foreach ($this->request->post as $key => $value) {
+        foreach ( $this->request->post as $key => $value ) {
             $params[] = $key . '=' . $value;
         }
 
-        $this->log->write('Humm Error: ' . $comment . ' (' . implode('; ', $params) . ')');
+        $this->log->write( 'Humm Error: ' . $comment . ' (' . implode( '; ', $params ) . ')' );
 
-        $this->response->addHeader($this->request->server['SERVER_PROTOCOL'] . ' 400 Bad Request');
+        $this->response->addHeader( $this->request->server['SERVER_PROTOCOL'] . ' 400 Bad Request' );
 
-        $this->response->addHeader('Content-type: application/json');
-        $this->response->setOutput(json_encode(["reference_id" => $reference_id, "status" => $comment]));
+        $this->response->addHeader( 'Content-type: application/json' );
+        $this->response->setOutput( json_encode( [ "reference_id" => $reference_id, "status" => $comment ] ) );
     }
 
     /**
@@ -124,7 +130,7 @@ class ControllerExtensionPaymentHumm extends Controller {
      *
      * @return mixed
      */
-    private function getOrderAndVerifyResponse($request) {
+    private function getOrderAndVerifyResponse( $request ) {
         $required = [
             'x_account_id',
             'x_reference',
@@ -137,26 +143,26 @@ class ControllerExtensionPaymentHumm extends Controller {
         ];
 
         // Required
-        foreach ($required as $key => $value) {
-            if (!isset($request[$key]) || empty($request[$key])) {
-                unset($required[$key]);
+        foreach ( $required as $key => $value ) {
+            if ( ! isset( $request[ $key ] ) || empty( $request[ $key ] ) ) {
+                unset( $required[ $key ] );
             }
         }
 
-        if (!empty($required)) {
-            throw new \Exception('Bad Request. Missing required fields: ' . implode(', ', $required) . '.');
+        if ( ! empty( $required ) ) {
+            throw new \Exception( 'Bad Request. Missing required fields: ' . implode( ', ', $required ) . '.' );
         }
 
         // Validate Signature
-        if (!$this->model_extension_payment_humm->validateSignature($request)) {
-            throw new \Exception('Bad Request. Unable to validate signature.');
+        if ( ! $this->model_extension_payment_humm->validateSignature( $request ) ) {
+            throw new \Exception( 'Bad Request. Unable to validate signature.' );
         }
 
-        $order_info = $this->model_checkout_order->getOrder($request['x_reference']);
+        $order_info = $this->model_checkout_order->getOrder( $request['x_reference'] );
 
         // Order Exists
-        if (empty($order_info)) {
-            throw new \Exception('Bad Request. Invalid Order ID.');
+        if ( empty( $order_info ) ) {
+            throw new \Exception( 'Bad Request. Invalid Order ID.' );
         }
 
         return $order_info;
@@ -165,10 +171,10 @@ class ControllerExtensionPaymentHumm extends Controller {
     /**
      * @param mixed[] $request
      */
-    private function updateOrder($order_info, $request) {
-        $order_status_id = $this->model_extension_payment_humm->getStatus($request['x_result']);
+    private function updateOrder( $order_info, $request ) {
+        $order_status_id = $this->model_extension_payment_humm->getStatus( $request['x_result'] );
 
-        if ($order_status_id == $order_info['order_status_id']) {
+        if ( $order_status_id == $order_info['order_status_id'] ) {
             return;
         }
 
@@ -179,23 +185,24 @@ class ControllerExtensionPaymentHumm extends Controller {
         $comment .= 'Gateway Reference: ' . $request['x_gateway_reference'] . "\n";
         $comment .= 'Amount: ' . $request['x_amount'] . "\n";
         $comment .= 'Currency: ' . $request['x_currency'] . "\n";
-        $comment = strip_tags($comment);
+        $comment = strip_tags( $comment );
 
-        $this->model_checkout_order->addOrderHistory($order_info['order_id'], $order_status_id, $comment, false);
+        $this->model_checkout_order->addOrderHistory( $order_info['order_id'], $order_status_id, $comment, false );
+
         return $request['x_result'];
     }
 
     /**
      * @param string $type
      */
-    private function debugLogIncoming($type) {
-        if (static::IS_DEBUG) {
-            $str = var_export([
-                'get' => $_GET,
+    private function debugLogIncoming( $type ) {
+        if ( static::IS_DEBUG ) {
+            $str = var_export( [
+                'get'  => $_GET,
                 'post' => $_POST,
-            ], true);
+            ], true );
 
-            $this->log->write('Humm ' . $type . ' Debug: ' . $str);
+            $this->log->write( 'Humm ' . $type . ' Debug: ' . $str );
         }
     }
 }
--- upload/catalog/model/extension/payment/humm.php
+++ upload/catalog/model/extension/payment/humm.php
@@ -1,4 +1,5 @@
 <?php
+
 class ModelExtensionPaymentHumm extends Model {
     /**
      * @param mixed[] $address
@@ -6,27 +7,27 @@ class ModelExtensionPaymentHumm extends Model {
      *
      * @return mixed[]
      */
-    public function getMethod($address, $total) {
-        $query = $this->db->query("SELECT * FROM " . DB_PREFIX . "zone_to_geo_zone WHERE geo_zone_id = '" . (int)$this->config->get('payment_humm_geo_zone_id') . "' AND country_id = '" . (int)$address['country_id'] . "' AND (zone_id = '" . (int)$address['zone_id'] . "' OR zone_id = '0')");
+    public function getMethod( $address, $total ) {
+        $query = $this->db->query( "SELECT * FROM " . DB_PREFIX . "zone_to_geo_zone WHERE geo_zone_id = '" . (int) $this->config->get( 'payment_humm_geo_zone_id' ) . "' AND country_id = '" . (int) $address['country_id'] . "' AND (zone_id = '" . (int) $address['zone_id'] . "' OR zone_id = '0')" );
 
         $status = false;
 
-        if (!$this->config->get('payment_humm_geo_zone_id')) {
+        if ( ! $this->config->get( 'payment_humm_geo_zone_id' ) ) {
             $status = true;
-        } elseif ($query->num_rows) {
+        } elseif ( $query->num_rows ) {
             $status = true;
         }
 
         $method_data = [];
 
-        if ($status) {
-            $title = $this->config->get('payment_humm_title');
+        if ( $status ) {
+            $title = $this->config->get( 'payment_humm_title' );
 
             $method_data = [
                 'code'       => 'humm',
-                'title'      => $this->config->get('payment_humm_title'),
-                'terms'      => $this->config->get('payment_humm_description'),
-                'sort_order' => $this->config->get('payment_humm_sort_order'),
+                'title'      => $this->config->get( 'payment_humm_title' ),
+                'terms'      => $this->config->get( 'payment_humm_description' ),
+                'sort_order' => $this->config->get( 'payment_humm_sort_order' ),
             ];
         }
 
@@ -40,20 +41,20 @@ class ModelExtensionPaymentHumm extends Model {
      *
      * @return string
      */
-    public function getSignature($params) {
+    public function getSignature( $params ) {
         $string = '';
 
-        ksort($params);
+        ksort( $params );
 
-        foreach ($params as $key => $value) {
-            if (substr($key, 0, 2) === 'x_') {
+        foreach ( $params as $key => $value ) {
+            if ( substr( $key, 0, 2 ) === 'x_' ) {
                 $string .= $key . $value;
             }
         }
 
-        $hash = hash_hmac('sha256', $string, $this->config->get('payment_humm_api_key'));
+        $hash = hash_hmac( 'sha256', $string, $this->config->get( 'payment_humm_api_key' ) );
 
-        return str_replace('-', '', $hash);
+        return str_replace( '-', '', $hash );
     }
 
     /**
@@ -63,16 +64,16 @@ class ModelExtensionPaymentHumm extends Model {
      *
      * @return bool
      */
-    public function validateSignature($params) {
-        if (!isset($params['x_signature'])) {
+    public function validateSignature( $params ) {
+        if ( ! isset( $params['x_signature'] ) ) {
             return false;
         }
 
         $signature = $params['x_signature'];
 
-        unset($params['x_signature']);
+        unset( $params['x_signature'] );
 
-        return $signature == $this->getSignature($params);
+        return $signature == $this->getSignature( $params );
     }
 
     /**
@@ -81,81 +82,80 @@ class ModelExtensionPaymentHumm extends Model {
      * @return mixed[]
      */
     public function getParams() {
-        $this->load->model('checkout/order');
+        $this->load->model( 'checkout/order' );
 
-        $order_info = $this->model_checkout_order->getOrder($this->session->data['order_id']);
+        $order_info = $this->model_checkout_order->getOrder( $this->session->data['order_id'] );
 
-        $payment_country_info = $this->db->query("SELECT * FROM " . DB_PREFIX . "country WHERE country_id = '" . (int)$order_info['payment_country_id'] . "' AND status = 1 LIMIT 1")->row;
-        $payment_zone_info = $this->db->query("SELECT * FROM " . DB_PREFIX . "zone WHERE zone_id = '" . (int)$order_info['payment_zone_id'] . "' AND status = 1 AND country_id = '" . (int)$order_info['payment_country_id'] . "' LIMIT 1")->row;
+        $payment_country_info = $this->db->query( "SELECT * FROM " . DB_PREFIX . "country WHERE country_id = '" . (int) $order_info['payment_country_id'] . "' AND status = 1 LIMIT 1" )->row;
+        $payment_zone_info    = $this->db->query( "SELECT * FROM " . DB_PREFIX . "zone WHERE zone_id = '" . (int) $order_info['payment_zone_id'] . "' AND status = 1 AND country_id = '" . (int) $order_info['payment_country_id'] . "' LIMIT 1" )->row;
 
-        $shipping_country_info = $this->db->query("SELECT * FROM " . DB_PREFIX . "country WHERE country_id = '" . (int)$order_info['shipping_country_id'] . "' AND status = 1 LIMIT 1")->row;
-        $shipping_zone_info = $this->db->query("SELECT * FROM " . DB_PREFIX . "zone WHERE zone_id = '" . (int)$order_info['shipping_zone_id'] . "' AND status = 1 AND country_id = '" . (int)$order_info['shipping_country_id'] . "' LIMIT 1")->row;
+        $shipping_country_info = $this->db->query( "SELECT * FROM " . DB_PREFIX . "country WHERE country_id = '" . (int) $order_info['shipping_country_id'] . "' AND status = 1 LIMIT 1" )->row;
+        $shipping_zone_info    = $this->db->query( "SELECT * FROM " . DB_PREFIX . "zone WHERE zone_id = '" . (int) $order_info['shipping_zone_id'] . "' AND status = 1 AND country_id = '" . (int) $order_info['shipping_country_id'] . "' LIMIT 1" )->row;
 
         $url_prefix = (
-            $this->config->get('site_ssl')
-            ? $this->config->get('site_ssl')
-            : $this->config->get('site_base')
+        $this->config->get( 'site_ssl' )
+            ? $this->config->get( 'site_ssl' )
+            : $this->config->get( 'site_base' )
         );
 
         $params = [
             // Required
-            'x_account_id' => $this->config->get('payment_humm_merchant_id'),
-            'x_amount' => $this->currency->format($order_info['total'], $order_info['currency_code'], $order_info['currency_value'], false),
-            'x_currency' => $order_info['currency_code'],
-            'x_reference' => $this->session->data['order_id'],
-            'x_shop_country' => $this->config->get('payment_humm_region'),
-            'x_shop_name' => $this->config->get('payment_humm_shop_name'),
-            'x_test' => 'false',
-            'x_url_callback' => $this->url->link('extension/payment/humm/callback', '', true),
+            'x_account_id'                   => $this->config->get( 'payment_humm_merchant_id' ),
+            'x_amount'                       => $this->currency->format( $order_info['total'], $order_info['currency_code'], $order_info['currency_value'], false ),
+            'x_currency'                     => $order_info['currency_code'],
+            'x_reference'                    => $this->session->data['order_id'],
+            'x_shop_country'                 => $this->config->get( 'payment_humm_region' ),
+            'x_shop_name'                    => $this->config->get( 'payment_humm_shop_name' ),
+            'x_test'                         => 'false',
+            'x_url_callback'                 => $this->url->link( 'extension/payment/humm/callback', '', true ),
             // Proxy files required as gateway doesn't append resulting request
             //  arguments to existing ones.
             // 'x_url_cancel' => $url_prefix . 'catalog/controller/extension/payment/humm-cancel.php',
             // 'x_url_complete' => $url_prefix . 'catalog/controller/extension/payment/humm-complete.php',
-            'x_url_cancel' => $this->url->link('extension/payment/humm/cancel', '', true),
-            'x_url_complete' => $this->url->link('extension/payment/humm/complete', '', true),
+            'x_url_cancel'                   => $this->url->link( 'extension/payment/humm/cancel', '', true ),
+            'x_url_complete'                 => $this->url->link( 'extension/payment/humm/complete', '', true ),
 
             // Optional
-            'x_customer_first_name' => $order_info['payment_firstname'],
-            'x_customer_last_name' => $order_info['payment_lastname'],
-            'x_customer_email' => $order_info['email'],
-            'x_customer_phone' => $order_info['telephone'],
-            'x_customer_billing_address1' => $order_info['payment_address_1'],
-            'x_customer_billing_address2' => $order_info['payment_address_2'],
-            'x_customer_billing_city' => $order_info['payment_city'],
-            'x_customer_billing_state' => '',
-            'x_customer_billing_postcode' => $order_info['payment_postcode'],
-            'x_customer_billing_country' => '',
+            'x_customer_first_name'          => $order_info['payment_firstname'],
+            'x_customer_last_name'           => $order_info['payment_lastname'],
+            'x_customer_email'               => $order_info['email'],
+            'x_customer_phone'               => $order_info['telephone'],
+            'x_customer_billing_address1'    => $order_info['payment_address_1'],
+            'x_customer_billing_address2'    => $order_info['payment_address_2'],
+            'x_customer_billing_city'        => $order_info['payment_city'],
+            'x_customer_billing_state'       => '',
+            'x_customer_billing_postcode'    => $order_info['payment_postcode'],
+            'x_customer_billing_country'     => '',
             'x_customer_shipping_first_name' => $order_info['shipping_firstname'],
-            'x_customer_shipping_last_name' => $order_info['shipping_lastname'],
-            'x_customer_shipping_address1' => $order_info['shipping_address_1'],
-            'x_customer_shipping_address2' => $order_info['shipping_address_2'],
-            'x_customer_shipping_city' => $order_info['shipping_city'],
-            'x_customer_shipping_state' => '',
-            'x_customer_shipping_postcode' => $order_info['shipping_postcode'],
-            'x_customer_shipping_country' => '',
-            'x_description' => 'Order #' . $order_info['order_id'],
-            'abc123' => '1111',
+            'x_customer_shipping_last_name'  => $order_info['shipping_lastname'],
+            'x_customer_shipping_address1'   => $order_info['shipping_address_1'],
+            'x_customer_shipping_address2'   => $order_info['shipping_address_2'],
+            'x_customer_shipping_city'       => $order_info['shipping_city'],
+            'x_customer_shipping_state'      => '',
+            'x_customer_shipping_postcode'   => $order_info['shipping_postcode'],
+            'x_customer_shipping_country'    => '',
+            'x_description'                  => 'Order #' . $order_info['order_id'],
+            'abc123'                         => '1111',
         ];
 
-        if ($payment_country_info) {
+        if ( $payment_country_info ) {
             $params['x_customer_billing_country'] = $payment_country_info['iso_code_2'];
         }
 
-        if ($payment_zone_info) {
+        if ( $payment_zone_info ) {
             $params['x_customer_billing_state'] = $payment_zone_info['code'];
         }
 
-        if ($shipping_country_info) {
+        if ( $shipping_country_info ) {
             $params['x_customer_shipping_country'] = $shipping_country_info['iso_code_2'];
         }
 
-        if ($shipping_zone_info) {
+        if ( $shipping_zone_info ) {
             $params['x_customer_shipping_state'] = $shipping_zone_info['code'];
         }
 
-        $params['x_signature'] = $this->getSignature($params);
+        $params['x_signature'] = $this->getSignature( $params );
 
-        
 
         return $params;
     }
@@ -165,9 +165,9 @@ class ModelExtensionPaymentHumm extends Model {
      */
     public function getStatuses() {
         return [
-            'completed' => $this->config->get('payment_humm_order_status_completed_id'),
-            'pending' => $this->config->get('payment_humm_order_status_pending_id'),
-            'failed' => $this->config->get('payment_humm_order_status_failed_id'),
+            'completed' => $this->config->get( 'payment_humm_order_status_completed_id' ),
+            'pending'   => $this->config->get( 'payment_humm_order_status_pending_id' ),
+            'failed'    => $this->config->get( 'payment_humm_order_status_failed_id' ),
         ];
     }
 
@@ -176,12 +176,12 @@ class ModelExtensionPaymentHumm extends Model {
      *
      * @return string|null
      */
-    public function getStatus($outcome) {
+    public function getStatus( $outcome ) {
         $statuses = $this->getStatuses();
 
         return (
-            isset($statuses[$outcome])
-            ? $statuses[$outcome]
+        isset( $statuses[ $outcome ] )
+            ? $statuses[ $outcome ]
             : 0
         );
     }
@@ -190,21 +190,21 @@ class ModelExtensionPaymentHumm extends Model {
      * @return string
      */
     public function getGatewayUrl() {
-        $environment = $this->config->get('payment_humm_gateway_environment');
+        $environment = $this->config->get( 'payment_humm_gateway_environment' );
 
-        if ($environment == 'other') {
-            return $this->config->get('payment_humm_gateway_url');
+        if ( $environment == 'other' ) {
+            return $this->config->get( 'payment_humm_gateway_url' );
         }
 
-        $region = $this->config->get('payment_humm_region');
+        $region = $this->config->get( 'payment_humm_region' );
 
-        if ($region == 'NZ') {
+        if ( $region == 'NZ' ) {
             $tld = 'co.nz';
         } else {
             $tld = 'com.au';
         }
 
-        if ($environment == 'live') {
+        if ( $environment == 'live' ) {
             $prefix = 'secure';
         } else {
             $prefix = 'securesandbox';
