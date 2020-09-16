be705adda8cd0b2964d384e5a93b31668e150b10
Roger Bi <roger.bi@flexigroup.com.au>
2020-09-16 18:09:28 +1000

    update

--- upload/catalog/language/en-gb/extension/payment/humm.php
+++ upload/catalog/language/en-gb/extension/payment/humm.php
@@ -1,11 +1,11 @@
 <?php
 // Text
-$_['text_transaction_failed']       = 'Your payment with humm/Oxipay has been declined.';
-$_['text_transaction_cancelled']    = 'Your payment with humm/Oxipay has been cancelled.';
-$_['text_transaction_verification'] = 'Your payment with humm/Oxipay had an error, please try again.';
+$_['text_transaction_failed']       = 'Your payment with Humm NZ  has been declined.';
+$_['text_transaction_cancelled']    = 'Your payment with HUMM NZ  has been cancelled.';
+$_['text_transaction_verification'] = 'Your payment with HUmm NZ  had an error, please try again.';
 
 // Button
-$_['button_confirm']                = 'Proceed to payment';
+$_['button_confirm']                = 'Proceed to Humm NZ';
 
 // Error
-$_['error_amount']                  = 'Orders under %s are not supported by humm/Oxipay. Please select a different payment option.';
+$_['error_amount']                  = 'Orders under %s are not supported Humm NZ. Please select a different payment option.';
